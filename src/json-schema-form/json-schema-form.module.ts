ca410d304503542c347872fb33a823da53e26a50
rand0me <not.randome@gmail.com>
2017-02-15 13:32:58 +0300

    chore(style): Fix import columns in json-schema-form.module.ts

--- src/json-schema-form/json-schema-form.module.ts
+++ src/json-schema-form/json-schema-form.module.ts
@@ -1,13 +1,12 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, ModuleWithProviders }    from '@angular/core';
+import { CommonModule }                     from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
+import { MaterialModule }                   from '@angular/material';
 
-import { JsonSchemaFormComponent } from './json-schema-form.component';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { JsonSchemaFormComponent }     from './json-schema-form.component';
+import { DynamicFormComponent }        from './dynamic-form/dynamic-form.component';
 import { DynamicFormControlComponent } from './dynamic-form-control/dynamic-form-control.component';
-
-import { JsonSchemaFormService } from './json-schema-form.service';
+import { JsonSchemaFormService }       from './json-schema-form.service';
 
 @NgModule({
     imports: [
